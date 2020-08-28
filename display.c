042ca4b86a0f147fbcbb47e29c9e69bbba4d35f8
JeroenvanBeek <JJ.vanbeek@student.han.nl>
2020-08-28 12:18:12 +0200

    Add first child with niceIncr

--- display.c
+++ display.c
@@ -26,9 +26,19 @@ if(err != NO_ERR) DisplayError(err);        // Print an error message
     numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
     niceIncr = (int) strtoul(argv[3], NULL, 10);
     
+switch(fork())
+{
+case -1:
+    printf("fork failed.\n");
+    break;
+
+//child process
+case 0:
+    nice(niceIncr * 1);
+    printChar = argv[5][0];
     PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
-  
-  
+    break;
+ } 
   printf("\n");  // Newline at end
   return 0;
 }
