310fe6d3de6fdcdf0924e306df0539098b6551ba
JeroenvanBeek <JJ.vanbeek@student.han.nl>
2020-08-28 20:40:39 +0200

    initial commit for ex04

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 # Makefile for OPS exercise 2
 # Use tabs for indentation, not spaces!
 
-all: printversion display
+all: printversion parent
 
 # Link object files and create a binary executable:
-display: display.o displayFunctions.o syntaxCheck.o
-	gcc -Wall -o display display.o displayFunctions.o   syntaxCheck.o
+parent: parent.o displayFunctions.o syntaxCheck.o
+	gcc -Wall -o parent parent.o displayFunctions.o   syntaxCheck.o
 
 # Compile C-files and create object files (.o).
 # The option -c ensures no linking takes place:
-display.o: display.c displayFunctions.h errorCodes.h
-	gcc -c -Wall display.c
+parent.o: parent.c displayFunctions.h errorCodes.h
+	gcc -c -Wall parent.c
 
 displayFunctions.o: displayFunctions.c displayFunctions.h syntaxCheck.h errorCodes.h
 	gcc -c -Wall displayFunctions.c
@@ -21,7 +21,7 @@ clean:
 	rm -f errorCodes.o
 
 cleanall: clean
-	rm -f display
+	rm -f parent
 
 printversion:
 	gcc --version | grep gcc
--- parent.c
+++ parent.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "displayFunctions.h"
 
 int main(int argc, char *argv[]) 
@@ -19,26 +20,54 @@ int main(int argc, char *argv[])
   ErrCode err;
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
-  
-if(err != NO_ERR) DisplayError(err);        // Print an error message
+
+if(err != NO_ERR) DisplayError(err); // Print an error message
   
     printMethod = argv[1][0];
     numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
     niceIncr = (int) strtoul(argv[3], NULL, 10);
+
     
 switch(fork())
 {
 case -1:
     printf("fork failed.\n");
-    break;
+    exit(1);
 
 //child process
 case 0:
     nice(niceIncr * 1);
     printChar = argv[5][0];
     PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
-    break;
+break;
+
+default:
+
+switch(fork())
+{
+case -1:
+    printf("fork failed.\n");
+    exit(1);
+    
+case 0:
+    nice(niceIncr * 2);
+    printChar = argv[6][0];
+    PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
+break;
+
+default:
+    
+    printChar = argv[4][0];
+    PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
+    
+      // Wait for two children:
+      wait(NULL);
+      wait(NULL);
+      printf("\n");  // Newline at end
+
+break;
+}
+break;    
  } 
-  printf("\n");  // Newline at end
-  return 0;
+return 0;
 }
