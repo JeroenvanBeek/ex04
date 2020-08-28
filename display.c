d0003e5538b575b6cdaed87b3206dd6cbffacaca
JeroenvanBeek <JJ.vanbeek@student.han.nl>
2020-08-28 12:09:19 +0200

    Add niceIncr

--- display.c
+++ display.c
@@ -8,23 +8,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "displayFunctions.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{ 
+  int niceIncr; 
   unsigned long int numOfTimes;
   char printMethod, printChar;
   ErrCode err;
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
-  if(err != NO_ERR) {
-    DisplayError(err);        // Print an error message
-  } else {
+  
+if(err != NO_ERR) DisplayError(err);        // Print an error message
+  
     printMethod = argv[1][0];
     numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
-    printChar = argv[3][0];
+    niceIncr = (int) strtoul(argv[3], NULL, 10);
     
     PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
-  }
+  
   
   printf("\n");  // Newline at end
   return 0;
